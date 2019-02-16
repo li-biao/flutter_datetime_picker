dab542109b0318ed37c1e9a110ddb1b92237c4a4
lib <183784016@qq.com>
2019-02-16 21:34:00 +0800

    只给自己用

---
+++ README.md
@@ -0,0 +1,57 @@
+#感谢此作者提供 https://github.com/Realank/flutter_datetime_picker
+# Flutter Datetime Picker
+
+[(Pub) flutter_datetime_picker](https://pub.dartlang.org/packages/flutter_datetime_picker)
+
+A flutter date time picker inspired by [flutter-cupertino-date-picker](https://github.com/wuzhendev/flutter-cupertino-date-picker)
+
+you can choose date / time / date&time in multiple languages:
+- English
+- Dutch
+- Chinese
+- Russian
+- Italian
+- French
+- Spanish
+
+and you can also custom your own picker content
+
+
+|Date picker|Time picker|Date Time picker|
+| ------- | ------- |------- |
+|![]( screen_date.png) |![]( screen_time.png) |![]( screen_datetime_chinese.png) |
+
+International:
+
+| Date Time picker (Chinese) | Date Time picker (America) | Date Time picker (Dutch) | Date Time picker (Russian) |
+| ------- | ------- | ------- | ------- |
+|![]( screen_datetime_chinese.png)|![]( screen_datetime_english.png)|![]( screen_datetime_dutch.png)|![]( screen_datetime_russian.png)|
+
+## Usage
+```
+FlatButton(
+    onPressed: () {
+        DatePicker.showDatePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime(2018, 3, 5),
+                              maxTime: DateTime(2019, 6, 7), onChanged: (date) {
+                            print('change $date');
+                          }, onConfirm: (date) {
+                            print('confirm $date');
+                          }, currentTime: DateTime.now(), locale: LocaleType.zh);
+    },
+    child: Text(
+        'show date time picker (Chinese)',
+        style: TextStyle(color: Colors.blue),
+    ));
+```
+
+## Custom
+If you want to customize your own style of date time picker, there is a class called CommonPickerModel, every type of date time picker is extended from this class, you can refer to other picker model (eg. DatePickerModel), and write your custom one, then pass this model to showPicker method, so that your own date time picker will appear, it’s easy, and will perfectly meet your demand
+## Getting Started
+
+For help getting started with Flutter, view our online [documentation](https://flutter.io/).
+
+For help on editing package code, view the [documentation](https://flutter.io/developing-packages/).
+
+## 在原作的基础上，粗暴的增加一个参数可以在选择年月日的时候，只选择年月
---
+++ example/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Datetime Picker'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+                onPressed: () {
+                  DatePicker.showDatePicker(context,
+                      showTitleActions: true,
+                      minTime: DateTime(2018, 3, 5),
+                      maxTime: DateTime(2019, 6, 7),
+                      theme: DatePickerTheme(
+                          backgroundColor: Colors.blue,
+                          itemStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                          doneStyle: TextStyle(color: Colors.white, fontSize: 16)),
+                      onChanged: (date) {
+                    print('change $date');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime.now(), locale: LocaleType.zh);
+                },
+                child: Text(
+                  'show date picker(custom theme &date time range)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            FlatButton(
+                onPressed: () {
+                  DatePicker.showTimePicker(context, showTitleActions: true, onChanged: (date) {
+                    print('change $date');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime(2008, 12, 31, 23, 12, 34));
+                },
+                child: Text(
+                  'show time picker',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            FlatButton(
+                onPressed: () {
+                  DatePicker.showDateTimePicker(context, showTitleActions: true, onChanged: (date) {
+                    print('change $date');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime(2008, 12, 31, 23, 12, 34), locale: LocaleType.zh);
+                },
+                child: Text(
+                  'show date time picker (Chinese)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            FlatButton(
+                onPressed: () {
+                  DatePicker.showDateTimePicker(context, showTitleActions: true, onChanged: (date) {
+                    print('change $date');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime(2008, 12, 31, 23, 12, 34));
+                },
+                child: Text(
+                  'show date time picker (English-America)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            FlatButton(
+                onPressed: () {
+                  DatePicker.showDateTimePicker(context, showTitleActions: true, onChanged: (date) {
+                    print('change $date');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime(2008, 12, 31, 23, 12, 34), locale: LocaleType.nl);
+                },
+                child: Text(
+                  'show date time picker (Dutch)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            FlatButton(
+                onPressed: () {
+                  DatePicker.showDateTimePicker(context, showTitleActions: true, onChanged: (date) {
+                    print('change $date');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime(2008, 12, 31, 23, 12, 34), locale: LocaleType.ru);
+                },
+                child: Text(
+                  'show date time picker (Russian)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
